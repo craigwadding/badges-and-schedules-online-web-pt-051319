141d770efb5be109abedaa9b95c19d03e562103e
craig wadding <craig.wadding@gmail.com>
2019-05-23 00:54:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,9 @@
 def badge_maker(name)
   "Hello, my name is #[name}"
 end 
+
+def batch_badge_creator(attendees)
+  attendees.each do |attendee|
+    puts "Hello, my name is #{attendee}"
+  end
+end
